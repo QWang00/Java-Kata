57bdf25b9b10f53b626353fc339c0cc25557c5c1
qiuqiu <qiujuanwang87@gmail.com>
2025-04-01 12:47:50 +0100

    test: implement tests for fidMissingNumbers in HashSetKataTests

--- src/HashSetKataTests.java
+++ src/HashSetKataTests.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HashSetKataTests {
 
@@ -57,5 +57,64 @@ public class HashSetKataTests {
             List<Integer> result = HashSetKata.removeDuplicates(input);
             assertEquals(Arrays.asList(-90,1,98,0), result);
         }
+    }
+
+    @Nested
+    class FindMissingNumbers{
+        @Test
+        @DisplayName("Should return empty list when the input list is empty")
+        void emptyInput(){
+            int[] inputList = {};
+            int n = 2;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertTrue(result.isEmpty());
+        }
+
+        @Test
+        @DisplayName("Should return empty list when the input list is null")
+        void nullInput(){
+            int[] inputList = null;
+            int n = 2;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertTrue(result.isEmpty());
+        }
+
+        @Test
+        @DisplayName("Should return empty list when there are no missing numbers in the input list")
+        void noMissing(){
+            int[] inputList = {1,2,3,4,5};
+            int n = 5;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertTrue(result.isEmpty());
+        }
+
+        @Test
+        @DisplayName("Should ignore extra numbers in the list when there are numbers larger than n")
+        void largerNumbersThanN(){
+            int[] inputList = {1,99};
+            int n = 5;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertEquals(List.of(2,3,4,5), result);
+        }
+
+        @Test
+        @DisplayName("Should handle unordered input the same as ordered input")
+        void unorderedList(){
+            int[] inputList = {5,3,2,4,1};
+            int n = 7;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertEquals(List.of(6,7), result);
+        }
+
+
+        @Test
+        @DisplayName("Should ignore duplicate numbers in the input")
+        void duplicatesNumbersInList(){
+            int[] inputList = {1,1,3,3,6,6,8};
+            int n = 9;
+            List<Integer> result = HashSetKata.findMissingNumbers(inputList, n);
+            assertEquals(List.of(2,4,5,7,9),result);
+        }
+
     }
 }
